b8b1fb1f6f1d6814e50dd11dfbbb2f1a6505541c
beezies <briannaxrogers@yahoo.com>
2024-10-17 03:49:48 -0700

    necessary fields added to new account page

--- src/FinanceUI/FinanceApp.java
+++ src/FinanceUI/FinanceApp.java
@@ -1,16 +1,22 @@
 package FinanceUI;
 
+import java.time.LocalDate;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class FinanceApp extends Application{
 	
 	Scene homeScene;
 	Scene newAccScene;
+	LocalDate date = LocalDate.now();
 	
 	@Override
     public void start(Stage stage) {
@@ -25,26 +31,42 @@ public class FinanceApp extends Application{
 	
 	public Scene getHomeScene(Stage stage) {
 		VBox pane = new VBox();
-		Button newAccButton = new Button("Add New Account");
-		Label label = new Label("To be implemented: home page \n(Add account button, \naccounts view, "
+		Button newAccBtn = new Button("Add New Account");
+		Label lbl = new Label("To be implemented: home page \n(Add account button, \naccounts view, "
 				+ "\nschedule transaction button, \ntransactions view)");
-		pane.getChildren().add(label);
-		pane.getChildren().add(newAccButton);
+		pane.getChildren().add(lbl);
+		pane.getChildren().add(newAccBtn);
 		
-		newAccButton.setOnAction(e -> stage.setScene(newAccScene));
-		return new Scene(pane, 300, 300);
+		newAccBtn.setOnAction(e -> stage.setScene(newAccScene));
+		return new Scene(pane, 450, 450);
 	}
 	
 	public Scene getNewAccScene(Stage stage) {
 		VBox pane = new VBox();
-		Button backButton = new Button("Cancel");
+		VBox fieldPane = new VBox();
+		HBox btnPane = new HBox();
+		
 		Label lbl = new Label("To be implemented: new account page \n(Account name,"
 				+ " \ncreation date, \nstarting balance)");
-		pane.getChildren().add(backButton);
-		pane.getChildren().add(lbl);
+		TextField nameTF = new TextField("Enter Account Name");
+		DatePicker dp = new DatePicker(date);
+		TextField balanceTF = new TextField("Enter Starting Balance");
 		
-		backButton.setOnAction(e -> stage.setScene(homeScene));
-		return new Scene(pane, 300, 300);
+		Button addBtn = new Button("Add Account");
+		Button cancelBtn = new Button("Cancel");
+		
+		fieldPane.getChildren().add(lbl);
+		fieldPane.getChildren().add(nameTF);
+		fieldPane.getChildren().add(dp);
+		fieldPane.getChildren().add(balanceTF);
+		btnPane.getChildren().add(addBtn);
+		btnPane.getChildren().add(cancelBtn);
+		pane.getChildren().add(fieldPane);
+		pane.getChildren().add(btnPane);
+		
+		addBtn.setOnAction(e -> stage.setScene(homeScene));
+		cancelBtn.setOnAction(e -> stage.setScene(homeScene));
+		return new Scene(pane, 450, 450);
 	}
 	
     public static void main(String[] args) {

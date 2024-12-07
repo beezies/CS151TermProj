bc895e69bf89033e5403222983f666d509b92bd7
beezies <briannaxrogers@yahoo.com>
2024-12-07 07:24:37 -0800

    separate ui

---
+++ src/application/UserInterface.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application{
+
+	@Override
+	public void start(Stage stage) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void showAlert(String title, String message) {
+		Alert alert = new Alert(Alert.AlertType.WARNING);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.showAndWait();
+	}
+
+}
